f191964b9298af91240b74baec928d75b8051c25
StuartCowley <voidstalker@live.co.uk>
2022-03-30 17:37:56 +0100

    Add Selection component

---
+++ src/components/Selection.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Selection = ({ choices }) => {
+  return (
+    <>
+      <h2 className="selection__title">Make a selection: </h2>
+      <div className="selection__option-wrap">
+        TODO: Add your options here
+      </div>
+    </>
+  );
+};
+
+export default Selection;
